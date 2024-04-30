90dd5bf1bd935490c37315346405a07d117a7358
kadorek <haltaskadir@gmail.com>
2024-04-30 11:36:34 +0300

    user authentication function and account controller added

---
+++ Fihrist/Controllers/AccountController.cs
@@ -0,0 +1,55 @@
+﻿using Fihrist.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Razor.Language.Intermediate;
+using System.Security.Claims;
+using System.Security.Cryptography.Pkcs;
+
+namespace Fihrist.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly FihristContext context;
+        public AccountController()
+        {
+            context = new FihristContext();
+        }
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Login([FromForm, Bind("Username", "Password")] User dataUser)
+        {
+            var user = context.Users.FirstOrDefault(x => x.Username == dataUser.Username && x.Password == dataUser.Password);
+            if (user == null)
+            {
+                return View(dataUser);
+            }
+
+            var claims = new List<Claim> {
+                new Claim(ClaimTypes.Email,user.Email),
+                new Claim(ClaimTypes.Name,user.Username),
+                new Claim("UserPassword",user.Password),
+                new Claim("UserId",user.Id.ToString())
+            };
+
+
+            var claimIdentity = new ClaimsIdentity(claims,CookieAuthenticationDefaults.AuthenticationScheme);
+            var claimPrincipal = new ClaimsPrincipal(claimIdentity);
+            await HttpContext.SignInAsync(claimPrincipal);
+
+            return RedirectToAction("Index","Home");
+        }
+
+    }
+}
--- Fihrist/Models/FihristContext.cs
+++ Fihrist/Models/FihristContext.cs
@@ -21,9 +21,11 @@ public partial class FihristContext : DbContext
 
     public virtual DbSet<Type> Types { get; set; }
 
+    public virtual DbSet<User> Users { get; set; }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlite("Data Source= .\\Data\\fihrist.db");
+        => optionsBuilder.UseSqlite("Data Source=.\\Data\\fihrist.db");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -48,6 +50,11 @@ public partial class FihristContext : DbContext
         modelBuilder.Entity<Type>(entity =>
         {
             entity.ToTable("Type");
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.ToTable("User");
         });
 
         OnModelCreatingPartial(modelBuilder);
